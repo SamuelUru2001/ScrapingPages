33eda32b695b19e769a83b90dc71c6cc1ecaf77e
samuelUru1402 <87658058+SamuelUru2109@users.noreply.github.com>
2023-01-10 17:41:27 -0300

    new page extract data...

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-const scrapingPage = require('./scriptScraping')
+const scrapingPage1 = require('./scriptScraping')
+const scrapingPage2 = require('./scraping2')
 const transformtoCsv = require('./csvExport')
 
 ;(async()=>{
 
-    await scrapingPage()
+    // await scrapingPage1()
 
-    await transformtoCsv()
+    await scrapingPage2()
+
+    // await transformtoCsv()
 
 })()
